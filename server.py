5cfb6c8fa4eb8254126f2942c7a62914fbdcbb53
SeoulDroid99100 <zekey8249+1@gmail.com>
2025-05-28 16:20:48 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,17 +1,1 @@
-# flask.py
-from flask import Flask
-from threading import Thread
 
-flask = Flask(__name__)  # Flask instance named 'flask'
-
-@flask.route('/')
-def index():
-    return "Flask server is running!"
-
-def run():
-    flask.run(host='0.0.0.0', port=7860)
-
-def start_flask():
-    thread = Thread(target=run)
-    thread.daemon = True
-    thread.start()
